d821e498e1a9c030711662040468f2c89ca77eb0
dariojr <dariojrodriguez2000@gmail.com>
2020-01-17 19:24:13 -0300

    **

--- tarea_flujo_exp/models/models.py
+++ tarea_flujo_exp/models/models.py
@@ -439,7 +439,7 @@ class expediente(models.Model):
                 # FIN NO SE PUEDE ENVIAR SI NO ESTA EN MI OFICINA
                 flujo_obj = self.env['tarea_flujo.flujo'].search([('name', '=', [proced_id])])
                 #print(("NO HAY FLUJO PARA ... " + expte_obj.procedimiento_id.name))
-                #SI NO HAY FLUJO O EL EXPEDIENTE TIENE UBICACION ACTUAL EN LA NUBE
+                #SI NO HAY FLUJO O EL EXPEDIENTE TIENE UBICACION ACTUAL EN LA NUBE--
                 if not flujo_obj or expte_obj.ubicacion_actual.name.lower() == "Nube".lower():
                         return self.enviar()
                 if flujo_obj:
